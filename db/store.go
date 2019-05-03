83af3ae05498bc11493329cdbb48023deac71ef2
Michael Mokrysz <michael.mokrysz@digital.cabinet-office.gov.uk>
2019-05-03 11:51:08 +0100

    Organisation and Space GUIDs not always present
    This commit is a quick workaround for some CF Audit Events not
    including an Organisation GUID or Space GUID.

--- db/store.go
+++ db/store.go
@@ -62,15 +62,47 @@ func (s *EventStore) StoreCfAuditEvents(events []cfclient.Event) error {
 	}
 	defer tx.Rollback()
 	for _, event := range events {
-		stmt := fmt.Sprintf(`
-			insert into %s (
-				guid, created_at, event_type, actor, actor_type, actor_name, actor_username, actee, actee_type, actee_name, organization_guid, space_guid
-			) values (
-				$1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12
-			) on conflict do nothing
-		`, CfAuditEventsTable)
-		_, err := tx.Exec(stmt, event.GUID, event.CreatedAt, event.Type, event.Actor, event.ActorType, event.ActorName, event.ActorUsername, event.Actee, event.ActeeType, event.ActeeName, event.OrganizationGUID, event.SpaceGUID)
-		return err
+		var err error
+		if event.OrganizationGUID != "" && event.SpaceGUID != "" {
+			stmt := fmt.Sprintf(`
+				insert into %s (
+					guid, created_at, event_type, actor, actor_type, actor_name, actor_username, actee, actee_type, actee_name, organization_guid, space_guid
+				) values (
+					$1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12
+				) on conflict do nothing
+			`, CfAuditEventsTable)
+			_, err = tx.Exec(stmt, event.GUID, event.CreatedAt, event.Type, event.Actor, event.ActorType, event.ActorName, event.ActorUsername, event.Actee, event.ActeeType, event.ActeeName, event.OrganizationGUID, event.SpaceGUID)
+		} else if event.OrganizationGUID != "" {
+			stmt := fmt.Sprintf(`
+				insert into %s (
+					guid, created_at, event_type, actor, actor_type, actor_name, actor_username, actee, actee_type, actee_name, organization_guid, space_guid
+				) values (
+					$1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, null::uuid
+				) on conflict do nothing
+			`, CfAuditEventsTable)
+			_, err = tx.Exec(stmt, event.GUID, event.CreatedAt, event.Type, event.Actor, event.ActorType, event.ActorName, event.ActorUsername, event.Actee, event.ActeeType, event.ActeeName, event.OrganizationGUID)
+		} else if event.SpaceGUID != "" {
+			stmt := fmt.Sprintf(`
+				insert into %s (
+					guid, created_at, event_type, actor, actor_type, actor_name, actor_username, actee, actee_type, actee_name, organization_guid, space_guid
+				) values (
+					$1, $2, $3, $4, $5, $6, $7, $8, $9, $10, null::uuid, $11
+				) on conflict do nothing
+			`, CfAuditEventsTable)
+			_, err = tx.Exec(stmt, event.GUID, event.CreatedAt, event.Type, event.Actor, event.ActorType, event.ActorName, event.ActorUsername, event.Actee, event.ActeeType, event.ActeeName, event.SpaceGUID)
+		} else {
+			stmt := fmt.Sprintf(`
+				insert into %s (
+					guid, created_at, event_type, actor, actor_type, actor_name, actor_username, actee, actee_type, actee_name, organization_guid, space_guid
+				) values (
+					$1, $2, $3, $4, $5, $6, $7, $8, $9, $10, null::uuid, null::uuid
+				) on conflict do nothing
+			`, CfAuditEventsTable)
+			_, err = tx.Exec(stmt, event.GUID, event.CreatedAt, event.Type, event.Actor, event.ActorType, event.ActorName, event.ActorUsername, event.Actee, event.ActeeType, event.ActeeName)
+		}
+		if err != nil {
+			return err
+		}
 	}
 	return tx.Commit()
 }
