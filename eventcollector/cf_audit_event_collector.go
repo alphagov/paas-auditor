29ae6c4ab6b14248c60b2d058f7f5d38c37768ce
Michael Mokrysz <michael.mokrysz@digital.cabinet-office.gov.uk>
2019-07-08 13:59:30 +0100

    Insert events to the database page-by-page
    Before this commit we fetched Audit Events from Cloud Controller using
    the `cfclient.ListEventsByQuery` function. This function fetches all
    the pages and then returns all the events. There are a few downsides:
    
      - This function fetched all events into memory and then we saved all
        events at once. On GOV.UK PaaS, it takes tens of thousands of API
        requests to fetch all pages. Just one HTTP or DB error during this
        process would waste all that work.
    
      - The function did not support ratelimiting, and the API call seems
        relatively expensive.
    
    These issues are both quite important to being production-ready. It
    is probably easier to move the fetching into our code than patch both
    of them in `cfclient`. We had to use channels to pass things around
    and that would be a major departure from the rest of the `cfclient`
    library.

--- eventcollector/cf_audit_event_collector.go
+++ eventcollector/cf_audit_event_collector.go
@@ -64,16 +64,16 @@ func (c *CfAuditEventCollector) Run(ctx context.Context) error {
 		select {
 		case <-time.After(c.waitDuration()):
 			startTime := time.Now()
-			collectedEvents, err := c.collect(ctx)
+			collectedEventsCount, err := c.collect(ctx)
 			if err != nil {
 				c.state = Scheduled
 				c.logger.Error("collect-error", err)
 				continue
 			}
 			elapsedTime := time.Since(startTime)
-			c.eventsCollected += len(collectedEvents)
+			c.eventsCollected += collectedEventsCount
 			c.logger.Info("collected", lager.Data{
-				"count":    len(collectedEvents),
+				"count":    collectedEventsCount,
 				"duration": elapsedTime.String(),
 			})
 		case <-ctx.Done():
@@ -83,19 +83,41 @@ func (c *CfAuditEventCollector) Run(ctx context.Context) error {
 }
 
 // collect reads a batch of RawEvents from the EventFetcher and writes them to the EventStore
-func (c *CfAuditEventCollector) collect(ctx context.Context) ([]cfclient.Event, error) {
-	events, err := c.fetcher.FetchEvents(ctx, c.lastRun)
-	if err != nil {
-		return nil, err
-	}
+func (c *CfAuditEventCollector) collect(ctx context.Context) (int, error) {
+	c.logger.Info("collect.start")
+	// Pull an extra 5 seconds of events, to ensure we don't miss any
+	pullEventsSince := c.lastRun.Add(-5 * time.Second)
+	eventsChan := make(chan []cfclient.Event)
+	errChan := make(chan error)
+	go c.fetcher.FetchEvents(ctx, pullEventsSince, eventsChan, errChan)
 
-	if err := c.store.StoreCfAuditEvents(events); err != nil {
-		return nil, err
+	eventsCount := 0
+	for {
+		select {
+		case events, stillOpen := <-eventsChan:
+			if !stillOpen {
+				c.logger.Info("collect.eventschan-closed")
+				break
+			}
+			eventsCount += len(events)
+			err := c.store.StoreCfAuditEvents(events)
+			if err != nil {
+				c.logger.Info("collect.store-error")
+				return eventsCount, err
+			}
+			c.logger.Info("collected.page")
+		case err, stillOpen := <-errChan:
+			if !stillOpen {
+				c.logger.Info("collect.errchan-closed")
+				break
+			}
+			return eventsCount, err
+		}
 	}
 
 	c.lastRun = time.Now()
 	c.state = Scheduled
-	return events, nil
+	return eventsCount, nil
 }
 
 // wait returns a channel that closes after the collection schedule time has elapsed
--- eventfetchers/cf_audit_event_fetcher.go
+++ eventfetchers/cf_audit_event_fetcher.go
@@ -2,6 +2,7 @@ package eventfetchers
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 	"net/url"
 	"time"
@@ -22,24 +23,60 @@ func NewCFAuditEventFetcher(client *cfclient.Client, logger lager.Logger) *CFAud
 	}
 }
 
-func (e *CFAuditEventFetcher) FetchEvents(ctx context.Context, lastRun time.Time) ([]cfclient.Event, error) {
+func (e *CFAuditEventFetcher) FetchEvents(ctx context.Context, pullEventsSince time.Time, eventsChan chan []cfclient.Event, errChan chan error) {
 	e.logger.Info("fetching", lager.Data{
-		"last_run": lastRun,
+		"pull_events_since": pullEventsSince,
 	})
 
-	timestamp := fmt.Sprintf("timestamp>%s", lastRun.Format("2006-01-02T15:04:05Z"))
+	timestamp := fmt.Sprintf("timestamp>%s", pullEventsSince.Format("2006-01-02T15:04:05Z"))
 	q := url.Values{}
 	q.Set("q", timestamp)
+	q.Set("results-per-page", "100")
+	requestURL := fmt.Sprintf("/v2/events?%s", q.Encode())
 
-	events, err := e.client.ListEventsByQuery(q)
-	if err != nil {
-		return nil, err
+	for {
+		events, nextURL, err := e.fetchEventsPage(ctx, requestURL)
+		if err != nil {
+			errChan <- err
+			break
+		}
+		e.logger.Info("fetched.page", lager.Data{
+			"pull_events_since": pullEventsSince,
+			"page_url":          requestURL,
+			"event_count":       len(events),
+		})
+
+		eventsChan <- events
+
+		if nextURL == "" {
+			break
+		}
+		requestURL = nextURL
+
+		time.Sleep(e.client.Config.PaginationDelay)
 	}
 
-	e.logger.Info("fetched", lager.Data{
-		"last_run":    lastRun,
-		"event_count": len(events),
-	})
+	close(errChan)
+	close(eventsChan)
+}
 
-	return events, nil
+func (e *CFAuditEventFetcher) fetchEventsPage(ctx context.Context, requestURL string) ([]cfclient.Event, string, error) {
+	r := e.client.NewRequest("GET", requestURL)
+	resp, err := e.client.DoRequest(r)
+	if err != nil {
+		return nil, "", fmt.Errorf("error requesting events: %s", err)
+	}
+	defer resp.Body.Close()
+
+	var eventResp cfclient.EventsResponse
+	if err := json.NewDecoder(resp.Body).Decode(&eventResp); err != nil {
+		return nil, "", fmt.Errorf("error unmarshaling events: %s", err)
+	}
+	var events []cfclient.Event
+	for _, e := range eventResp.Resources {
+		e.Entity.GUID = e.Meta.Guid
+		e.Entity.CreatedAt = e.Meta.CreatedAt
+		events = append(events, e.Entity)
+	}
+	return events, eventResp.NextURL, nil
 }
