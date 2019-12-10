1988521b2c27bb7c1c7e7e5b9f3fe7011e62a6bf
Toby Lorne <toby.lornewelch-richards@digital.cabinet-office.gov.uk>
2019-12-10 12:07:59 +0000

    (re)-implement happy path for collector
    Signed-off-by: Toby Lorne <toby.lornewelch-richards@digital.cabinet-office.gov.uk>
    Co-authored-by: Rafal <rafal.proszowski@digital.cabinet.office.gov.uk>

--- pkg/collectors/cf_audit_event_collector.go
+++ pkg/collectors/cf_audit_event_collector.go
@@ -2,7 +2,6 @@ package collectors
 
 import (
 	"context"
-	"fmt"
 	"time"
 
 	"code.cloudfoundry.org/lager"
@@ -15,82 +14,83 @@ type CfAuditEventCollector struct {
 	schedule        time.Duration
 	logger          lager.Logger
 	fetcher         fetchers.CFAuditEventFetcher
-	eventDB         *db.EventStore
+	eventDB         db.EventDB
 	eventsCollected int
 }
 
-func NewCfAuditEventCollector(schedule time.Duration, logger lager.Logger, fetcher fetchers.CFAuditEventFetcher, eventDB *db.EventStore) *CfAuditEventCollector {
+func NewCfAuditEventCollector(
+	schedule time.Duration,
+	logger lager.Logger,
+	fetcher fetchers.CFAuditEventFetcher,
+	eventDB db.EventDB,
+) *CfAuditEventCollector {
 	logger = logger.Session("cf-audit-event-collector")
 	return &CfAuditEventCollector{schedule, logger, fetcher, eventDB, 0}
 }
 
 func (c *CfAuditEventCollector) Run(ctx context.Context) error {
+	lsession := c.logger.Session("run")
+	lsession.Info("start")
+	defer lsession.Info("end")
+
 	for {
-		c.logger.Info("collect.start")
 		pullEventsSince, err := c.pullEventsSince(5 * time.Second)
 		if err != nil {
-			c.logger.Error("collect.fetch.err", err)
+			lsession.Error("pull-events-since", err)
 			return err
 		}
 
-		c.logger.Info("collect.fetch", lager.Data{"pull_events_since": pullEventsSince})
-		startTime := time.Now()
-		err = c.collect(ctx, pullEventsSince)
-		if err != nil {
-			c.logger.Error("collect.fetch.err", err)
-			return err
-		}
-
-		c.logger.Info("collect.done", lager.Data{"fetch_duration": time.Since(startTime)})
 		select {
+		case <-ctx.Done():
+			lsession.Info("done")
+			return nil
 		case <-time.After(c.schedule):
 			continue
-		case <-ctx.Done():
-			c.logger.Info("context.done")
-			return nil
+		default:
+			startTime := time.Now()
+
+			resultsChan := make(chan fetchers.CFAuditEventResult, 3)
+			go c.fetcher(pullEventsSince, resultsChan)
+
+			var events []cfclient.Event
+
+			for result := range resultsChan {
+				if result.Err != nil {
+					lsession.Error("err-recv-events", err)
+					return result.Err
+				}
+
+				events = append(events, result.Events...)
+			}
+
+			err := c.eventDB.StoreCfAuditEvents(events)
+			if err != nil {
+				lsession.Error("err-store-cf-audit-events", err)
+				return err
+			}
+			c.eventsCollected += len(events)
+
+			lsession.Info(
+				"stored-events",
+				lager.Data{
+					"duration":         time.Since(startTime),
+					"events-collected": c.eventsCollected,
+				},
+			)
 		}
 	}
 }
 
 func (c *CfAuditEventCollector) pullEventsSince(overlapBy time.Duration) (time.Time, error) {
-	// latestCFEventTime, err := c.eventDB.GetLatestCfEventTime()
-	// if err != nil {
-	// 	return time.Now(), err
-	// }
+	latestCFEventTime, err := c.eventDB.GetLatestCfEventTime()
 
-	// var pullEventsSince time.Time
-	// if latestCFEventTime != nil {
-	// 	pullEventsSince = (*latestCFEventTime).Add(-overlapBy)
-	// }
-	return time.Now(), fmt.Errorf("Unimplemented") // TODO(paroxp, tlwr)
-}
-
-func (c *CfAuditEventCollector) collect(ctx context.Context, pullEventsSince time.Time) error {
-	c.logger.Info("collect.start")
-
-	resultsChan := make(chan fetchers.CFAuditEventResult, 3)
-	go c.fetcher(pullEventsSince, resultsChan)
-
-	for {
-		var events []cfclient.Event
-
-		select {
-		case <-ctx.Done():
-			return nil
-		case result, stillOpen := <-resultsChan:
-			if !stillOpen {
-				return nil
-			}
-			if result.Err != nil {
-				return result.Err
-			}
-			events = result.Events
-		}
-
-		err := c.eventDB.StoreCfAuditEvents(events)
-		if err != nil {
-			return err
-		}
-		c.eventsCollected += len(events)
+	if err != nil {
+		return time.Time{}, err
 	}
+
+	if latestCFEventTime == nil {
+		return time.Time{}, nil
+	}
+
+	return (*latestCFEventTime).Add(-overlapBy), nil
 }
