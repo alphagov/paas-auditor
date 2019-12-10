1e3b4be02d643935acc05ff7be0400f6fe861645
Toby Lorne <toby.lornewelch-richards@digital.cabinet-office.gov.uk>
2019-12-10 12:24:47 +0000

    Store events as we go instead of all at once
    Otherwise we are holding many events in memory for no reason
    
    Signed-off-by: Toby Lorne <toby.lornewelch-richards@digital.cabinet-office.gov.uk>
    Co-authored-by: Rafal <rafal.proszowski@digital.cabinet.office.gov.uk>

--- pkg/collectors/cf_audit_event_collector.go
+++ pkg/collectors/cf_audit_event_collector.go
@@ -7,7 +7,6 @@ import (
 	"code.cloudfoundry.org/lager"
 	"github.com/alphagov/paas-auditor/pkg/db"
 	"github.com/alphagov/paas-auditor/pkg/fetchers"
-	cfclient "github.com/cloudfoundry-community/go-cfclient"
 )
 
 type CfAuditEventCollector struct {
@@ -52,26 +51,31 @@ func (c *CfAuditEventCollector) Run(ctx context.Context) error {
 			resultsChan := make(chan fetchers.CFAuditEventResult, 3)
 			go c.fetcher(pullEventsSince, resultsChan)
 
-			var events []cfclient.Event
-
 			for result := range resultsChan {
 				if result.Err != nil {
 					lsession.Error("err-recv-events", err)
 					return result.Err
 				}
 
-				events = append(events, result.Events...)
-			}
+				err := c.eventDB.StoreCfAuditEvents(result.Events)
+				if err != nil {
+					lsession.Error("err-store-cf-audit-events", err)
+					return err
+				}
 
-			err := c.eventDB.StoreCfAuditEvents(events)
-			if err != nil {
-				lsession.Error("err-store-cf-audit-events", err)
-				return err
+				c.eventsCollected += len(result.Events)
+
+				lsession.Info(
+					"stored-events",
+					lager.Data{
+						"duration":         time.Since(startTime),
+						"events-collected": c.eventsCollected,
+					},
+				)
 			}
-			c.eventsCollected += len(events)
 
 			lsession.Info(
-				"stored-events",
+				"stored-all-events",
 				lager.Data{
 					"duration":         time.Since(startTime),
 					"events-collected": c.eventsCollected,
